83daeb7c7ebc054ca7211267d853072de915e8d4
Earl Wood <earl.wood@hotmail.com>
2021-03-07 21:28:02 -0600

    rutas funcionales in app.js file

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-//Rutas
+//Rutas funcionales
 
 app.use(require('./routes/login'));
 app.use(require('./routes/ventas'));
